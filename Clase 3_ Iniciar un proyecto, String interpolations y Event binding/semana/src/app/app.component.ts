d6adebb646310a30acb5651cf7d14d588523e5e6
kunisugi <jav.navarrete@duocuc.cl>
2022-08-23 15:52:42 -0400

    Lito

---
+++ Clase 3_ Iniciar un proyecto, String interpolations y Event binding/semana/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public nombre: string = 'Julio';
+  public contador: number = 0;
+  public scroll: number = 0;
+  public alto: number= 0;
+  public ancho: number= 0;
+  public imagen: string = 'https://images.hola.com/imagenes/mascotas/20180925130054/consejos-para-cuidar-a-un-gatito-recien-nacido-cs/0-601-526/cuidardgatito-t.jpg';
+  public sumar(n1: number, n2: number): number {
+    return n1 + n2;
+  }
+  public eventoBoton(event: Event): void {
+    console.log(event);
+    alert('Hola');
+  }
+  public aumentarContador(): void {
+    this.contador = this.contador + 1;
+  }
+  public scrollEn(event: Event): void {
+    this.scroll = (event.target as Element).scrollTop;
+  }
+  public capturarAlto(event: Event): void {
+    const valor = Number.parseInt((event.target as HTMLInputElement).value)
+    this.alto = valor;
+  }
+  public capturarAncho(event: Event): void {
+    const valor = Number.parseInt((event.target as HTMLInputElement).value)
+    this.ancho = valor;
+  }
+}
