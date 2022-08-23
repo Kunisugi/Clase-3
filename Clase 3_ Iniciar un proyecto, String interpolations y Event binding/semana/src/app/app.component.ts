fbcab0f5ef25e5c904db917adce2440fa8eea211
kunisugi <jav.navarrete@duocuc.cl>
2022-08-23 18:04:58 -0400

    Terminado

--- Clase 3_ Iniciar un proyecto, String interpolations y Event binding/semana/src/app/app.component.ts
+++ Clase 3_ Iniciar un proyecto, String interpolations y Event binding/semana/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   public alto: number= 300;
   public ancho: number= 300;
   public mostrar: boolean = false;
+  public color: string = "";
   public imagen: string = 'https://images.hola.com/imagenes/mascotas/20180925130054/consejos-para-cuidar-a-un-gatito-recien-nacido-cs/0-601-526/cuidardgatito-t.jpg';
   public sumar(n1: number, n2: number): number {
     return n1 + n2;
@@ -34,4 +35,10 @@ export class AppComponent {
     const valor = Number.parseInt((event.target as HTMLInputElement).value)
     this.ancho = valor;
   }
+
+  public capturarColor(event: Event): void{
+    const color = (event.target as HTMLInputElement).value;
+    this.color = color;
+  }
+
 }
